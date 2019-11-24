a4cc88602dd0a8077ee1dd149806bb5c9548c1cf
mrfigoro <mrfigoro@gmail.com>
2019-11-25 01:10:51 +0300

    adding routering for admin

--- shop/resources/views/welcome.blade.php
+++ shop/resources/views/welcome.blade.php
@@ -4,11 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'Домик Мастеров') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="{{ asset('public/favicon.ico') }}">
         <!-- Styles -->
         <style>
             html, body {
@@ -81,18 +82,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Домик Мастеров
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="http://domikmasterov.zzz.com.ua/">Сайт</a>
+                    <a href="https://localhost/admin/">Админка</a>
+                    <a href="https://github.com/MrFigoro/shop">GitHub</a>
                 </div>
             </div>
         </div>
---
+++ shop/routes/admin.php
@@ -0,0 +1,16 @@
+<?php
+Route::get('/loginform', 'AuthController@showLoginForm')->name('loginform');
+Route::post('/signin', 'AuthController@login')->name('signin');
+Route::post('/logout', 'AuthController@logout')->name('logout')->middleware('auth');
+
+Route::get('/', 'UserController@index')->name('index')->middleware('auth');
+Route::get('/users', 'UserController@index')->name('users')->middleware('auth');
+Route::get('/users/active/{id}', 'UserController@active')->name('usersActive')->middleware('auth');
+Route::post('/users', 'UserController@search')->name('usersSearch')->middleware('auth');
+Route::get('/users/create', 'UserController@create')->name('usersCreate')->middleware('auth');
+Route::post('/users/store', 'UserController@store')->name('usersSave')->middleware('auth');
+Route::get('/users/edit/{id?}', 'UserController@edit')->name('usersEdit')->middleware('auth');
+Route::get('/users/delete/{id}', 'UserController@destroy')->name('usersDelete')->middleware('auth');
+
+Route::get('/pages', 'PageController@index')->name('pages')->middleware('auth');
+Route::post('/pages', 'PageController@all')->name('allPages')->middleware('auth');
