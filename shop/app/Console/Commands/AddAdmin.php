26d7c59306239936b88abdbc959fc479c68c3881
mrfigoro <mrfigoro@gmail.com>
2019-12-14 16:53:29 +0300

    deleting admin migration and addig creating admin command

---
+++ shop/app/Console/Commands/AddAdmin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class AddAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'add:admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creating admin user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $email = $this->ask('Enter email for admin please:');
+    }
+}
